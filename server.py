242e9eb962aaec7572cee60c445903038102fae6
Yasyf Mohamedali <yasyfm@gmail.com>
2024-04-04 21:51:21 -0700

    perf: optimize server performance configurations
    - Increase the number of worker processes in the server configuration from `4` to `6`.
    
    Signed-off-by: Yasyf Mohamedali <yasyfm@gmail.com>

--- server.py
+++ server.py
@@ -58,7 +58,7 @@ if __name__ == "__main__":
         "--timeout",
         "600",
         "-w",
-        "4",
+        "6",
         "-b",
         f"{args.host}:{args.port}",
         "--log-level",
